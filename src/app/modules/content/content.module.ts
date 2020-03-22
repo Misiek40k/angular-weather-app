7ff467821eaced11b6beb194c60a2389905ebd3b
Misiek40k <comanderrr@gmail.com>
2020-03-22 14:22:27 +0100

    Add input component and Change baseUrl

--- src/app/modules/content/content.module.ts
+++ src/app/modules/content/content.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContentComponent } from './content.component';
+import { InputComponent } from './components/input/input.component';
 
 @NgModule({
-  declarations: [ContentComponent],
+  declarations: [ContentComponent, InputComponent,],
   imports: [
     CommonModule,
   ],
